6e244bf0146b3267dc6fa6d05633942d194eca4e
sydney33333 <166308944+sydney33333@users.noreply.github.com>
2024-12-29 04:23:48 -0800

    Add files via upload

---
+++ index.html.py
@@ -0,0 +1,18 @@
+
+<html>
+    <h1>
+        Hey, I'm codenerd
+    </h1>
+    <body>
+        <h2>About me</h2>
+        <p>
+            I am a car enthusiast  who helps people get there dream cars.
+        <p>
+        <h2>What I'm working on</h2>
+        <p>
+            I'm obsessed with AI right now.
+        </p>
+    </body>
+</html>
+
+
